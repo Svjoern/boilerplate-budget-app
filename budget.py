69c641bc75a1200c4bad7d65ffc66875783c15db
Sven <Sven_Kuehne@hotmail.de>
2022-02-08 21:11:39 +0000

    working on tasks in readme.md

--- budget.py
+++ budget.py
@@ -1,7 +1,50 @@
 class Category:
+  #food, clothin, entertainment
+  def __init__(self, name, balance=0):
+    self.name = name
+    self.balance = balance
+    self.ledger = []
 
+  def deposit(self, amount, description=""):
+    self.balance += amount
+    t_obj = {"amount": amount, "description": description}
+    #print(type(t_obj))
+    self.ledger.append(t_obj)
 
+  def withdraw(self, amount, description=""):
+    if self.check_funds(amount):
+      self.balance -= amount
+      t_obj = {"amount": -amount, "description": description}
+      self.ledger.append(t_obj)
+      return True
+    else:
+      return False
 
+  def get_balance(self):
+    return self.balance
+
+  def transfer(self, amount, Category):
+    if self.withdraw(amount, "Transfer to " + Category.name):
+      Category.deposit(amount, "Transfer from " + self.name)
+      return True
+    else:
+      return False
+
+  def check_funds(self, amount):
+    if (amount > self.balance):
+      return False
+    else:
+      return True
+
+  def __str__(self):
+    res = ""
+    star = "*"
+    tmp = int((30 - len(self.name)) / 2)
+    #print(tmp)
+    for i in range(tmp):
+      res += star
+    return res + self.name + res
 
 
 def create_spend_chart(categories):
+  pass
